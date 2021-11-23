1015e9d10583cdae1bff6f5bf106b689ab8db6ca
remi <remi.tondeux@gmail.com>
2021-11-23 14:05:56 +0100

    add users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Van.destroy_all if Rails.env.development?
+
 
 puts 'Cleaning database...'
-puts "Creating lists..."
+Van.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
+
+puts "Creating random users !"
+
+User.create!(email: "florimond@gmail.com", password: "azerty", first_name: "Florimond", last_name: "Billiau", driving_license: "false", address: "rue Royale")
+User.create!(email: "florimonde@gmail.com", password: "azerty", first_name: "Florimonde", last_name: "Billiot", driving_license: "false", address: "rue de la soif")
+User.create!(email: "fx@gmail.com", password: "azerty", first_name: "FX", last_name: "Wagon", driving_license: false, address: "Champs Elysees")
+
+puts "Creating awesome vans !"
 
 Van.create(nb_seats: 5, brand: "Volkswagen", description: "A nice van to escape", localisation: "Lille", van_url: "https://images.unsplash.com/photo-1527786356703-4b100091cd2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmFuJTIwbGlmZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60")
 Van.create(nb_seats: 4, brand: "Toyota", description: "A cool van to travel", localisation: "Paris", van_url: "https://images.unsplash.com/photo-1610851252127-85442ca5528e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHZhbiUyMGxpZmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60")
