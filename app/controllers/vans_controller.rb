952614238e18fba40fe57c12a00a53509cca6098
marwa homsi <marwa.9101@hotmail.com>
2021-11-23 11:14:59 +0100

    create a new van

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -2,4 +2,15 @@ class VansController < ApplicationController
   def index
     @vans = Van.all
   end
+  def new
+    @van = Van.new
+  end
+  def create
+    @van = Van.create(params_van)
+    redirect_to van_path(@van)
+  end
+  private
+  def params_van
+    params.require(:van).permit(:nb_seats, :brand, :description, :localisation)
+  end
 end
