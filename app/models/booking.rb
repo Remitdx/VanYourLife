33f2fbde15f493dd80443c9137d9070a6d070f51
Remitdx <89589062+Remitdx@users.noreply.github.com>
2021-11-23 14:24:10 +0100

    Update booking.rb
    adding 3 choices for validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :vans
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :status
+  validates :status, presence: true, inclusion: { in: ["Pending", "Accepted", "Declined"] }
 end
