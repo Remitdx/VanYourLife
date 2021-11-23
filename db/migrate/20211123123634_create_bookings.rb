25d3079c15a2bd5e92740fcb78728994cfed12fe
remi <remi.tondeux@gmail.com>
2021-11-23 14:50:56 +0100

    fixing migrations issues with booking model

--- db/migrate/20211123123634_create_bookings.rb
+++ db/migrate/20211123123634_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
-      t.user_id :references
-      t.van_id :references
+      t.references :van, foreign_key: true
+      t.references :user, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.string :status
