cdceaf55c824b76c8d6c6b0a645bdb2cda12f5ae
Remitdx <89589062+Remitdx@users.noreply.github.com>
2021-11-23 14:26:35 +0100

    Update user.rb
    uniqueness was too restrictive for testing purpose. moreover 2 user could have the same name or address (brothers for example)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :vans
 
-  validates :address, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true
-  validates :password, uniqueness: true, presence: true
-  validates :first_name, uniqueness: true, presence: true
-  validates :last_name, uniqueness: true, presence: true
-  validates :driving_license, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :driving_license, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
