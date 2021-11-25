308c05354131d46309bf4bc564b868ca89334bde
mathieu hollande <mathieuhollande@hotmail.fr>
2021-11-25 16:22:56 +0100

    add calendar for booking

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
@@ -24,9 +25,12 @@ import "bootstrap";
 // Internal imports, e.g:
 import { loadDynamicBannerText } from '../components/banner';
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
+
   loadDynamicBannerText();
 });
